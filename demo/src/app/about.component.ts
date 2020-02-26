d5eced8cacf9f2130de1a4dc8a24e15c34b929c6
Rin Minase <rin.black.raison.detre@gmail.com>
2020-02-27 00:25:00 +0800

    Added about and installation pages

---
+++ demo/src/app/about.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-about",
+  template: `
+    <p>
+      about works!
+    </p>
+  `,
+  styles: [],
+})
+export class AboutComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ demo/src/app/installation.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-installation",
+  template: `
+    <p>
+      installation works!
+    </p>
+  `,
+  styles: [],
+})
+export class InstallationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
