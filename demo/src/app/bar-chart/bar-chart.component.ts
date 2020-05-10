264e763af40947b92363168cdda0ffe161d84d18
Rin Minase <rin.black.raison.detre@gmail.com>
2020-05-10 10:08:37 +0800

    Modified bar chart in demo project

--- demo/src/app/bar-chart/bar-chart.component.ts
+++ demo/src/app/bar-chart/bar-chart.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { ChartOptions, ChartDataSets } from "chart.js";
 import { Label } from "@rinminase/ng-charts";
-import * as chartlabels from "chartjs-plugin-datalabels";
+
+import { AppService } from "@app/app.service";
 
 @Component({
     selector: "app-bar-chart",
     templateUrl: "./bar-chart.component.html",
 })
-export class BarChartComponent implements OnInit {
-    options: ChartOptions = {
+export class BarChartComponent {
+    chartOptions: ChartOptions = {
         responsive: true,
         scales: { xAxes: [{}], yAxes: [{}] },
         plugins: {
@@ -18,20 +19,26 @@ export class BarChartComponent implements OnInit {
             },
         },
     };
-    labels: Label[] = ["2006", "2007", "2008", "2009", "2010", "2011", "2012"];
-    plugins = [chartlabels];
-    data: ChartDataSets[] = [
+    chartLabels: Label[] = [
+        "2006",
+        "2007",
+        "2008",
+        "2009",
+        "2010",
+        "2011",
+        "2012",
+    ];
+    chartLegend = true;
+    chartPlugins = [];
+    chartData: ChartDataSets[] = [
         { data: [65, 59, 80, 81, 56, 55, 40], label: "Series A" },
         { data: [28, 48, 40, 19, 86, 27, 90], label: "Series B" },
     ];
 
-    constructor() {}
+    constructor(private service: AppService) {}
 
-    ngOnInit(): void {}
-
+    // Disregard this function, as this just randomizes the values in an array
     randomizer(): void {
-        const randomize = (): number => Math.round(Math.random() * 100);
-        const data = [randomize(), 59, 80, randomize(), 56, randomize(), 40];
-        this.data[0].data = data;
+        this.chartData[0].data = this.service.randomize(this.chartData[0].data);
     }
 }
