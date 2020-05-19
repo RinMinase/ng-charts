c0714c1c49cd1809eef022e750430eb09d762953
Rin Minase <rin.black.raison.detre@gmail.com>
2020-05-19 10:17:23 +0800

    Modified line chart in demo project

--- demo/src/app/line-chart/line-chart.component.ts
+++ demo/src/app/line-chart/line-chart.component.ts
@@ -69,31 +69,31 @@ export class LineChartComponent {
     };
     chartColors: Color[] = [
         {
-            // gray
-            backgroundColor: "rgba(148,159,177,0.2)",
-            borderColor: "rgba(148,159,177,1)",
-            pointBackgroundColor: "rgba(148,159,177,1)",
+            // green
+            backgroundColor: "rgba(76, 175, 80, 0.2)",
+            borderColor: "rgba(76, 175, 80, 1)",
+            pointBackgroundColor: "rgba(76, 175, 80, 1)",
             pointBorderColor: "#fff",
             pointHoverBackgroundColor: "#fff",
-            pointHoverBorderColor: "rgba(148,159,177,0.8)",
+            pointHoverBorderColor: "rgba(76, 175, 80, 0.8)",
         },
         {
-            // dark grey
-            backgroundColor: "rgba(77,83,96,0.2)",
-            borderColor: "rgba(77,83,96,1)",
-            pointBackgroundColor: "rgba(77,83,96,1)",
+            // blue
+            backgroundColor: "rgba(33, 150, 243, 0.2)",
+            borderColor: "rgba(33, 150, 243, 1)",
+            pointBackgroundColor: "rgba(33, 150, 243, 1)",
             pointBorderColor: "#fff",
             pointHoverBackgroundColor: "#fff",
-            pointHoverBorderColor: "rgba(77,83,96,1)",
+            pointHoverBorderColor: "rgba(33, 150, 243, 1)",
         },
         {
             // red
-            backgroundColor: "rgba(255,0,0,0.3)",
-            borderColor: "red",
-            pointBackgroundColor: "rgba(148,159,177,1)",
+            backgroundColor: "rgba(244, 67, 54, 0.3)",
+            borderColor: "rgba(244, 67, 54, 1)",
+            pointBackgroundColor: "rgba(244, 67, 54, 1)",
             pointBorderColor: "#fff",
             pointHoverBackgroundColor: "#fff",
-            pointHoverBorderColor: "rgba(148,159,177,0.8)",
+            pointHoverBorderColor: "rgba(244, 67, 54, 0.8)",
         },
     ];
     chartLegend = true;
