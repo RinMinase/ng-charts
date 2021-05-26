29c9fd2765ca5039aa94b8ededa0249b8bfcb635
Rin Minase <rin.black.raison.detre@gmail.com>
2021-05-26 17:21:13 +0800

    Appended polar chart in demo project

--- demo/src/app/polar-chart/polar-chart.component.ts
+++ demo/src/app/polar-chart/polar-chart.component.ts
@@ -1,11 +1,27 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+
+import { AppService } from "@app/app.service";
 
 @Component({
     selector: "app-polar-chart",
     templateUrl: "./polar-chart.component.html",
 })
-export class PolarChartComponent implements OnInit {
-    constructor() {}
+export class PolarChartComponent {
+    chartLabels = [
+        "Download Sales",
+        "In-Store Sales",
+        "Mail Sales",
+        "Telesales",
+        "Corporate Sales",
+    ];
 
-    ngOnInit(): void {}
+    chartData = [300, 500, 100, 40, 120];
+    chartLegend = true;
+
+    constructor(private service: AppService) {}
+
+    // Disregard this function, as this just randomizes the values in an array
+    randomizer(): void {
+        this.chartData = this.service.randomize(this.chartData);
+    }
 }
