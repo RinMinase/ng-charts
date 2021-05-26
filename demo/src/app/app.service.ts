304403c19f0b0599be625c9c44eacdfbeda5b38d
Rin Minase <rin.black.raison.detre@gmail.com>
2021-05-26 17:37:48 +0800

    Appended scatter chart randomizer in demo project

--- demo/src/app/app.service.ts
+++ demo/src/app/app.service.ts
@@ -23,6 +23,15 @@ export class AppService {
                     r: Math.round(Math.random() * 50),
                 });
             });
+        } else if (specialChart === "scatter") {
+            const rndPosNeg = () => Math.round(Math.random()) * 2 - 1;
+
+            values.forEach(() => {
+                returnValue.push({
+                    x: Math.round(Math.random() * 10 * rndPosNeg()),
+                    y: Math.round(Math.random() * 10 * rndPosNeg()),
+                });
+            });
         }
 
         return returnValue;
