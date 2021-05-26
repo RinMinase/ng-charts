553791b9f9289c09e1abd7c51bf3b029107950dc
Rin Minase <rin.black.raison.detre@gmail.com>
2021-05-26 17:02:58 +0800

    Appended radar chart in demo project

--- demo/src/app/radar-chart/radar-chart.component.ts
+++ demo/src/app/radar-chart/radar-chart.component.ts
@@ -1,11 +1,35 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+
+import { AppService } from "@app/app.service";
 
 @Component({
     selector: "app-radar-chart",
     templateUrl: "./radar-chart.component.html",
 })
-export class RadarChartComponent implements OnInit {
-    constructor() {}
+export class RadarChartComponent {
+    chartOptions = {
+        responsive: true,
+    };
 
-    ngOnInit(): void {}
+    chartLabels = [
+        "Eating",
+        "Drinking",
+        "Sleeping",
+        "Designing",
+        "Coding",
+        "Cycling",
+        "Running",
+    ];
+
+    chartData = [
+        { data: [65, 59, 90, 81, 56, 55, 40], label: "Series A" },
+        { data: [28, 48, 40, 19, 96, 27, 100], label: "Series B" },
+    ];
+
+    constructor(private service: AppService) {}
+
+    // Disregard this function, as this just randomizes the values in an array
+    randomizer(): void {
+        this.chartData[0].data = this.service.randomize(this.chartData[0].data);
+    }
 }
